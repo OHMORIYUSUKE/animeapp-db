32ad7970405d0b7a915d97c409c0d4c38ec43ca8
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2022-09-28 17:38:33 +0900

    スクレイピング完了

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,26 @@
+import superagent from "superagent";
+import cheerio from "cheerio";
+
 import { fizzbuzz } from "./lib/fizzbuzz";
 
 const num = 3;
 console.log(fizzbuzz(num));
+
+class Crowller {
+  private url = "https://qiita.com/suzuki_sh/items/f3349efbfe1bdfc0c634";
+  constructor() {
+    this.getRawHtml();
+  }
+  async getRawHtml() {
+    const result = await superagent.get(this.url);
+    this.getJobInfo(result.text);
+  }
+
+  getJobInfo(html: string) {
+    const $ = cheerio.load(html);
+    const text = $("h1.css-188vyrl").text();
+    console.log(text);
+  }
+}
+
+const crowller = new Crowller();
